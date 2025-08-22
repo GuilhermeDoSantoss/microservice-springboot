2eae4f28d76950fffa9646700938ee1c6d0eb25c
Guilherme dos Santos <guilherme1996santos@icloud.com>
2025-08-21 23:25:00 -0300

    configurando o RabbitMQ

--- src/main/java/com/micro/springboot/controller/PedidoController.java
+++ src/main/java/com/micro/springboot/controller/PedidoController.java
@@ -2,6 +2,8 @@ package com.micro.springboot.controller;
 
 import com.micro.springboot.model.Pedido;
 import com.micro.springboot.service.PedidoService;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,17 +12,23 @@ import java.util.List;
 @RequestMapping("/pedidos")
 public class PedidoController {
 
-
+    private final RabbitTemplate rabbitTemplate;
     private final PedidoService pedidoService;
 
 
-    public PedidoController(PedidoService pedidoService) {
+    public PedidoController(RabbitTemplate rabbitTemplate, PedidoService pedidoService) {
+        this.rabbitTemplate = rabbitTemplate;
         this.pedidoService = pedidoService;
     }
 
+    @Value("${broker.queue.processamento.name}")
+    private String routingKey;
+
     @PostMapping
     public String criarPedido(@RequestBody Pedido pedido){
-        pedidoService.salvarPedido(pedido);
+        Pedido pedidoSalvo = pedidoService.salvarPedido(pedido);
+        rabbitTemplate.convertAndSend("", routingKey, pedidoSalvo.getDescricao());
+
         return "Pedido salvo e envvviado para processamento:" + pedido.getDescricao();
     }
 
